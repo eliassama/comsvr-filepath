a0b55916009bc2486a224577b4e2c0adebe5aad0
CocytusElias <elias@elias.ink>
2022-01-07 11:05:15 +0800

    fix: 调整 eslint 相关内容

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,11 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['eliassama'],
+  extends: ['eliassama/typescript'],
   overrides: [
     {
       files: ['*.ts'],
       extends: ['eliassama'],
-      rules: {
-        // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md
-        'no-use-before-define': 'off',
-        '@typescript-eslint/no-use-before-define': ['error'],
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': ['error'],
-      },
     },
   ],
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
-  },
 };
